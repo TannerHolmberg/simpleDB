34a78e2c9a489d8def9537e59bad57429e89c696
TannerHolmberg <tannerholmberg@my.unt.edu>
2026-03-04 10:36:48 -0600

    Added a dedicated parser function to break up commands and validate input. Also added a switch case for main to run certain commands

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -14,25 +13,35 @@ func main() {
 	//For large input, increase the buffer size
 	in.Buffer(make([]byte, 0, 64*1024), 1024*1024)
 
+	//Temp in-memory store for key-value pairs for validation and retrieval
+	mem := make(map[string]string)
+
 	for in.Scan() {
 		//Trim leading and trailing whitespace
-		line := strings.TrimSpace(in.Text())
+		line := in.Text()
 
-		//Skip empty lines
-		if line == "" {
+		//Parse the input line into command, key, value, and validity
+		cmd, key, value, ok := parseInput(line)
+
+		if !ok {
 			continue
 		}
 
-		//Check for exit command (case-insensitive)
-		if strings.EqualFold(line, "EXIT") {
-			return
+		if cmd == "EXIT" {
+			break
 		}
 
-		//Error handling
-		if err := in.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
+		switch cmd {
+		case "GET":
+			if val, exists := mem[key]; exists {
+				fmt.Println(val)
+			}
+		case "SET":
+			mem[key] = value
+		case "Invalid":
+			continue
 		}
+
 	}
 
 }
---
+++ parser.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"strings"
+)
+
+func parseInput(input string) (Command string, key string, value string, ok bool) {
+	//Trim leading and trailing whitespace
+	line := strings.TrimSpace(input)
+	//Check for exit command (case-insensitive)
+	if strings.EqualFold(line, "EXIT") {
+		return "EXIT", "", "", false
+	}
+
+	//Split the line into parts
+	parts := strings.Fields(line)
+
+	if len(parts) == 0 {
+		return "Invalid", "", "", true
+	}
+
+	command := parts[0]
+	switch command {
+	case "GET":
+		if len(parts) < 2 {
+			return "Invalid", "", "", true
+		}
+		return "GET", parts[1], "", true
+	case "SET":
+		if len(parts) < 3 {
+			return "Invalid", "", "", true
+		}
+		return "SET", parts[1], parts[2], true
+	default:
+		return "Invalid", "", "", true
+	}
+}
